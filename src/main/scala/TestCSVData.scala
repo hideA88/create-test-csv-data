2ba8c35ec7b0db6a52812321126394aa119b10ab
Hideaki Tarumi <hideaki_pc@me.com>
2016-07-14 23:48:54 +0900

    csvからサンプルデータを抜き出すところを別メソッドに切り出す

--- src/main/scala/TestCSVData.scala
+++ src/main/scala/TestCSVData.scala
@@ -6,14 +6,19 @@ import java.io._
 object TestCsvData {
   def main(args: Array[String]): Unit = {
     val conf = TestCsvConf.getInstance(args)
+    val (header, sample) = getSampleData(conf)
+    val testData = createCopyData(sample, conf)
+    dumpCsvData(header, testData, conf)
+  }
+
+  def getSampleData(conf: TestCsvConf): (Seq[String], Map[String, String]) = {
     val reader = CSVReader.open(new File(conf.fileName), conf.encoding)
     val it = reader.iterator
     val header =  if(it.hasNext)  it.next else List()
     val data =  if(it.hasNext)  it.next else List()
     val sample = header.zip(data).toMap
-    val testData = createCopyData(sample, conf)
-    dumpCsvData(header, testData, conf)
-  }
+    (header, sample)
+   }
 
   def createCopyData(sample: Map[String, String],  conf: TestCsvConf): List[List[String]]= {
     val incIds = conf.incrementIds
