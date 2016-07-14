061fba7e941a21875148204f7568757ae397cbe2
Hideaki Tarumi <hideaki_pc@me.com>
2016-07-14 19:36:35 +0900

    作業途中のコミット

--- src/main/scala/TestCSVData.scala
+++ src/main/scala/TestCSVData.scala
@@ -2,19 +2,26 @@ import com.github.tototoshi.csv._
 import java.io._
 object TestCsvData {
   def main(args: Array[String]): Unit = {
-    val f = new TestCsvConf("sjis", "hoge.csv", List("id", "title"))
-    println(f)
     val conf = TestCsvConf.getInstance(args)
     val reader = CSVReader.open(new File(conf.fileName), conf.encoding)
     val it = reader.iterator
     val header =  if(it.hasNext)  it.next else List()
-    val sample =  if(it.hasNext)  it.next else List()
-    val data = createCopyData(sample, header, conf)
-    dumpCsvData(header, data)
+    val data =  if(it.hasNext)  it.next else List()
+    val sample = header.foldLeft(Map()){ (map, v) => }
+    val testData = createCopyData(sample, header, conf)
+    dumpCsvData(header, testData)
   }
 
-  def createCopyData(sample: Seq[String], header: Seq[String], conf: TestCsvConf): List[List[String]]= {
-
+  def createCopyData(sample: Map[String, String], header: Seq[String], conf: TestCsvConf): List[List[String]]= {
+    val incIds = conf.incrementIds
+    val data =  for( i <- 1 to conf.recordSize; col <- header) yield {
+      if(incIds.contains(col)){
+        col+i
+      }else{
+        col
+      }
+    }
+    println("data: "+data)
     return List(List("test"))
   }
 
--- src/main/scala/TestCsvConf.scala
+++ src/main/scala/TestCsvConf.scala
@@ -1,8 +1,11 @@
-case class TestCsvConf(encoding: String, fileName: String,
-  incrementIds: List[String])
+case class TestCsvConf private(encoding: String,
+   fileName: String,
+   recordSize: Int,
+   incrementIds: List[String]
+ )
 
 object  TestCsvConf {
   def getInstance(args: Array[String]): TestCsvConf = {
-    new TestCsvConf("sjis", "sample.csv", List("id", "title"))
+    new TestCsvConf("sjis", "sample.csv", 10,  List("id", "title"))
   }
 }
