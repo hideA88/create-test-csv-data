d36252b38f2143cc3a16fc30dd702e078d7d2336
Hideaki Tarumi <hideaki_pc@me.com>
2016-07-14 23:37:56 +0900

    csvを作成するところまで作成

--- src/main/scala/TestCSVData.scala
+++ src/main/scala/TestCSVData.scala
@@ -1,5 +1,8 @@
+package com.github.hideA88.testcsvdata
+
 import com.github.tototoshi.csv._
 import java.io._
+
 object TestCsvData {
   def main(args: Array[String]): Unit = {
     val conf = TestCsvConf.getInstance(args)
@@ -7,25 +10,31 @@ object TestCsvData {
     val it = reader.iterator
     val header =  if(it.hasNext)  it.next else List()
     val data =  if(it.hasNext)  it.next else List()
-    val sample = header.foldLeft(Map()){ (map, v) => }
-    val testData = createCopyData(sample, header, conf)
-    dumpCsvData(header, testData)
+    val sample = header.zip(data).toMap
+    val testData = createCopyData(sample, conf)
+    dumpCsvData(header, testData, conf)
   }
 
-  def createCopyData(sample: Map[String, String], header: Seq[String], conf: TestCsvConf): List[List[String]]= {
+  def createCopyData(sample: Map[String, String],  conf: TestCsvConf): List[List[String]]= {
     val incIds = conf.incrementIds
-    val data =  for( i <- 1 to conf.recordSize; col <- header) yield {
-      if(incIds.contains(col)){
-        col+i
-      }else{
-        col
-      }
+    val header = sample.keySet
+    val data = for( i <- 1 to conf.recordSize) yield {
+      header.map{ col =>
+        sample.get(col) match{
+          case Some(v) if incIds.contains(col) => v + i
+          case Some(v) if !incIds.contains(col) => v
+          case _ => ""
+        }
+      }.toList
     }
-    println("data: "+data)
-    return List(List("test"))
+    data.toList
   }
 
-  def dumpCsvData(header: Seq[String], data: List[List[String]]) = {
+  def dumpCsvData(header: Seq[String], data: List[List[String]], conf:TestCsvConf) = {
+    val file = new File("out.csv")
+    val writer = CSVWriter.open(file)
+    writer.writeAll(header :: data)
+    writer.close
     println("dumpCsvData")
   }
 }
--- src/main/scala/TestCsvConf.scala
+++ src/main/scala/TestCsvConf.scala
@@ -1,3 +1,5 @@
+package com.github.hideA88.testcsvdata
+
 case class TestCsvConf private(encoding: String,
    fileName: String,
    recordSize: Int,
@@ -6,6 +8,6 @@ case class TestCsvConf private(encoding: String,
 
 object  TestCsvConf {
   def getInstance(args: Array[String]): TestCsvConf = {
-    new TestCsvConf("sjis", "sample.csv", 10,  List("id", "title"))
+    new TestCsvConf("sjis", "sample.csv", 100,  List("id", "title"))
   }
 }
